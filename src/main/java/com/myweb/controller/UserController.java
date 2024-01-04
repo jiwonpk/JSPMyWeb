943a9da0f5ec17a9150c39a4552e8a2e9387b1ed
jiwon <piig1996@naver.com>
2024-01-04 17:50:04 +0900

    커밋

---
+++ src/main/java/com/myweb/controller/UserController.java
@@ -0,0 +1,73 @@
+package com.myweb.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.myweb.user.service.UserService;
+import com.myweb.user.service.UserServiceImpl;
+
+@WebServlet("*.user") //확장자패턴
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public UserController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//3. URL주소를 분기(각 요청별로 코드를 만듬)
+		request.setCharacterEncoding("utf-8"); //한글처리
+		
+		String uri = request.getRequestURI();
+		String path = uri.substring( request.getContextPath().length()  );
+		
+		System.out.println(path);
+		
+		
+		//서비스 영역 선언
+		UserService service = new UserServiceImpl();
+		
+		if(path.equals("/user/join.user")) { //가입화면
+			
+			//화면이동 기본값 - forward형식이 되어야함
+			request.getRequestDispatcher("user_join.jsp").forward(request, response);
+
+		} else if(path.equals("/user/login.user") ) { //로그인화면
+
+			request.getRequestDispatcher("user_login.jsp").forward(request, response);
+		
+		} else if(path.equals("/user/joinForm.user") ) { //회원가입
+			
+			//String id = request.getParameter("id");
+			//.....
+			int result = service.join(request, response);
+			
+			System.out.println("실행결과:" + result);
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	
+}
---
+++ src/main/java/com/myweb/user/model/UserDAO.java
@@ -0,0 +1,79 @@
+package com.myweb.user.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.myweb.util.JdbcUtil;
+
+public class UserDAO {
+	
+	//싱글톤
+	//1. 객체 1개 생성
+	private static UserDAO instance = new UserDAO();
+	//2. 생성자 private
+	private UserDAO() {
+		
+		try {
+			//드라이버 호출문장
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+		} catch (Exception e) {
+		}
+		
+	}
+	//3. getter메서드
+	public static UserDAO getInstance() {
+		return instance;
+	}
+	
+	/////////////////////////////////////////////
+	private String url = JdbcUtil.url;
+	private String uid = JdbcUtil.uid;
+	private String upw = JdbcUtil.upw;
+	
+	
+	public int idCheck(String id) {
+		int result = 0;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql = "select * from users where id = ?";
+		
+		try {
+			//conn객체생성
+			conn = DriverManager.getConnection(url, uid, upw);
+			//pstmt객체생성
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			//sql실행 -> select는 executeQuery, DML -> executeUpdate
+			rs = pstmt.executeQuery();
+			
+			if( rs.next() ) { //true라는 것은 -> 결과가 있다는 뜻
+				result = 1; //중복의 의미
+			} else { //false라는 것은 -> 중복된 결과가 없다는 뜻
+				result = 0; //중복이 x
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(conn, pstmt, rs);
+		}
+		return result;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
